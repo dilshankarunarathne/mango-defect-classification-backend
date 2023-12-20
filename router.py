18eb2ebe39f628f8f2bbad84a42ea291927da15a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-20 14:49:31 +0530

    Update router.py

--- router.py
+++ router.py
@@ -12,4 +12,4 @@ router = APIRouter(
 @router.post("/")
 async def classify(file: UploadFile = File(...)):
     prediction = await predict(file)
-    return {"message": "Hello World"}
+    return {"prediction": "Hello World"}
