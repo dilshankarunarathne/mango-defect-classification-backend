d693f5514b3976a1c1d56988242fd4622c81a970
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-20 15:25:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ from router import router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+
 app.include_router(router)
 
