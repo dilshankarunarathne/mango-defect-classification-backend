bb1fae82d3d5ef8b054204a41dda90d0686b470b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-20 14:42:12 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -8,7 +8,7 @@ model = tf.keras.models.load_model('mango.h5')
 
 def predict(img_path):
     # Load the image
-    img_path = 'test.jpg'
+    # img_path = 'test.jpg'
     img = image.load_img(img_path, target_size=(150, 150))
 
     # Preprocess the image
