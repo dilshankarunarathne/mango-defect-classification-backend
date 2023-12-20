f75ebc602f808ed6f68942f4e9f0f242a74104d3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-20 14:38:52 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -8,3 +8,4 @@ model = tf.keras.models.load_model('mango.h5')
 # Load the image
 img_path = 'test.jpg'  # replace with the path of your image
 img = image.load_img(img_path, target_size=(150, 150))
+
