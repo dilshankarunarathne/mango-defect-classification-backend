f4e3ad58ef869cb725902a1b6164b6249bc9a473
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-20 14:34:02 +0530

    Delete token_service.py

--- services/token_service.py
+++
@@ -1,9 +0,0 @@
-from services.database_service import dao
-
-
-def add_token_to_blacklist(token: str):
-    dao.blacklist_token(token)
-
-
-def check_if_token_is_blacklisted(token: str) -> bool:
-    return dao.is_token_blacklisted(token)
